4d4f3c754b92554b8d8e60f18a0f1a8bce465f33
Emilio Cantu <emiliocantu.500@gmail.com>
2021-03-23 14:14:50 -0600

    arregle problemas de async - sol fea

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ var map;
 var service;
 var distanceService;
 var infoWindow;
-var alcaldias = { "Álvaro Obregón": true, "Benito Juárez": true, "Coyoacán": true, "Cuajimalpa": false, "Cuauhtémoc": true, "Gustavo A. Madero": false, "Iztacalco": true, "Iztapalapa": true };
-var query = "abarrotes,oxxo,seven,k circle en ";
+var alcaldias = { "Álvaro Obregón": true, "Benito Juárez": false, "Coyoacán": true, "Cuajimalpa": false, "Cuauhtémoc": true, "Gustavo A. Madero": false, "Iztacalco": true, "Iztapalapa": true };
+var query = "en ";
 var posUsuario;
-var alcaldiaActual;
-var ultimaAlcaldia;
 var porAlcaldia = {};
 var todos = [];
 var aDesplegar = []; //top 5 de resultados x alcaldia. 5xno. de alcandias sin ley seca.
-var ultimoDesplegar;
-var duraciones={}; //de aDesplegar;
-var duracionesTxt={};
-var distanciasTxt={};
-var testResponse;
+var duraciones = {}; //de aDesplegar;
+var duracionesTxt = {};
+var distanciasTxt = {};
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 19.428607379617496, lng: -99.16786785469343 },
@@ -40,18 +36,18 @@ function initMap() {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude,
                     };
- 
+
                     createUserPosMarker(pos);
                     map.setCenter(pos);
-                    posUsuario=pos;
+                    posUsuario = pos;
                     distanceService = new google.maps.DistanceMatrixService();
-                    document.getElementById("resultados").innerHTML="";
-                    porAlcaldia={};
-                    todos=[];
-                    aDesplegar=[];
-                    duraciones={};
-                    duracionesTxt={};
-                    distanciasTxt={};
+                    document.getElementById("resultados").innerHTML = "";
+                    porAlcaldia = {};
+                    todos = [];
+                    aDesplegar = [];
+                    duraciones = {};
+                    duracionesTxt = {};
+                    distanciasTxt = {};
                     getBoozeData(map, pos);
 
                 },
@@ -91,30 +87,30 @@ function createMarker(place) {
 function createUserPosMarker(pos) {
     const markerUsuario = new google.maps.Marker({
         map,
-        position:pos,
-        icon:"http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+        position: pos,
+        icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
     });
 }
 function displayResult(resultado) {
-    var place_id=resultado['place_id'];
-    var duration=duracionesTxt[place_id];
-    var distance=distanciasTxt[place_id];
+    var place_id = resultado['place_id'];
+    var duration = duracionesTxt[place_id];
+    var distance = distanciasTxt[place_id];
     console.log("displaying:");
     console.log(place_id);
     console.log(resultado);
     console.log("with durations:");
     console.log(duracionesTxt);
-    console.log("data:"+duration+","+distance);
+    console.log("data:" + duration + "," + distance);
     var linkMaps = "https://www.google.com/maps/dir/?api=1&destination=CDMX&destination_place_id=" + resultado['place_id'] + "&travelmode=car";
     var card = document.createElement("div");
-    card.setAttribute("id",place_id);
+    card.setAttribute("id", place_id);
     card.setAttribute("class", "card");
     var cardBody = document.createElement("div");
     cardBody.setAttribute("class", "card-body");
     var title = document.createElement("h5");
     title.innerHTML = resultado['name'];
     var body = document.createElement("p");
-    body.innerHTML = '<span class="badge badge-success">'+duration+'</span> <span class="badge badge-primary">'+distance+'</span><br>'+resultado['formatted_address']+".";
+    body.innerHTML = '<span class="badge badge-success">' + duration + '</span> <span class="badge badge-primary">' + distance + '</span><br>' + resultado['formatted_address'] + ".";
     var link = document.createElement("a");
     link.setAttribute("class", "card-link");
     link.setAttribute("href", linkMaps);
@@ -127,24 +123,23 @@ function displayResult(resultado) {
     console.log(card);
     document.getElementById("resultados").appendChild(card);
 }
-function getDistanceAndDisplay(resultado){
+function getDistanceAndDisplay(resultado) {
     distanceService.getDistanceMatrix(
         {
-          origins: [posUsuario],
-          destinations: [resultado['geometry']['location']],
-          travelMode: 'DRIVING',
-        }, (response,status)=>{
-            if(status!=="OK"){
-                console.log("error en "+resultado+status);
-                alert("Error: "+staus);
-            }else{
+            origins: [posUsuario],
+            destinations: [resultado['geometry']['location']],
+            travelMode: 'DRIVING',
+        }, (response, status) => {
+            if (status !== "OK") {
+                console.log("error en " + resultado + status);
+                alert("Error: " + staus);
+            } else {
                 console.log(response);
-                testResponse=response;
-                duraciones[resultado['place_id']]=response['rows'][0]['elements'][0]['duration']['value'];
-                duracionesTxt[resultado['place_id']]=response['rows'][0]['elements'][0]['duration']['text'];
-                distanciasTxt[resultado['place_id']]=response['rows'][0]['elements'][0]['distance']['text'];
-                if(resultado==ultimoDesplegar){
-                    aDesplegar=aDesplegar.sort((f,s)=>duraciones[f['place_id']]-duraciones[s['place_id']]);
+                duraciones[resultado['place_id']] = response['rows'][0]['elements'][0]['duration']['value'];
+                duracionesTxt[resultado['place_id']] = response['rows'][0]['elements'][0]['duration']['text'];
+                distanciasTxt[resultado['place_id']] = response['rows'][0]['elements'][0]['distance']['text'];
+                if (Object.keys(duraciones).length==aDesplegar.length) {
+                    aDesplegar = aDesplegar.sort((f, s) => duraciones[f['place_id']] - duraciones[s['place_id']]);
                     console.log(duracionesTxt);
                     console.log(distanciasTxt);
                     console.log(aDesplegar);
@@ -159,44 +154,44 @@ function showResultados() {
     for (alcaldia in porAlcaldia) {
         aDesplegar = aDesplegar.concat(porAlcaldia[alcaldia].slice(0, 5));
     }
-    ultimoDesplegar=aDesplegar[aDesplegar.length-1];
     for (resultado of aDesplegar) {
         getDistanceAndDisplay(resultado); //displayResult
     }
 }
 function getBoozeData(map, loc, callback) {
-    var listaDeAlcaldias=Object.keys(alcaldias).filter((k)=>!alcaldias[k]); //alcaldias sin ley seca
-    ultimaAlcaldia=listaDeAlcaldias[listaDeAlcaldias.length-1];
+    var listaDeAlcaldias = Object.keys(alcaldias).filter((k) => !alcaldias[k]); //alcaldias sin ley seca
+    ultimaAlcaldia = listaDeAlcaldias[listaDeAlcaldias.length - 1];
+    console.log(listaDeAlcaldias);
     for (alcaldia of listaDeAlcaldias) {
-        if (alcaldias[alcaldia] == false) {//no tiene ley seca
-            alcaldiaActual = alcaldia;
-            getBoozeAlcaldiaData(map, alcaldia);
-        }
+        request = {
+            query: query + alcaldia,
+            type: ['liquor_store'],
+            openNow: true,
+            rankBy: google.maps.places.RankBy.DISTANCE,
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, (results, status) => {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                    todos.push(results[i]);
+                }
+                //getAlcaldiaActual by iterating over porAlcaldia and check witch empty
+                for(alc of listaDeAlcaldias){
+                    if(porAlcaldia[alc]==undefined){
+                        alcaldiaActual=alc;
+                    }
+                };
+                porAlcaldia[alcaldiaActual] = results;
+                if(Object.keys(porAlcaldia).length==listaDeAlcaldias.length){
+                    showResultados();
+                }
+            }
+        });
+
     }
 }
-function getBoozeAlcaldiaData(map, alcaldia) {
-    request = {
-        query: query + alcaldia,
-        type: ['liquor_store'],
-        openNow: true,
-        rankBy: google.maps.places.RankBy.DISTANCE,
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, textSearchCallback);
-}
-function textSearchCallback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-            console.log(results[i]);
-            todos.push(results[i]);
-        }
-        porAlcaldia[alcaldiaActual] = results;
-        if(alcaldiaActual==ultimaAlcaldia){
-            showResultados();
-        }
-    }
-}
+
 //directions url:
 /**
  * https://www.google.com/maps/dir/?api=1&destination=CDMX&destination_place_id=ChIJyxqEYnAB0oUREuiroM4kP0U&travelmode=car
